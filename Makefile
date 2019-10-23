87ef1af7ff78f1166b332aa173a17592e334f888
Hrishi Olickel <hrishioa@gmail.com>
2019-10-23 23:37:55 +0800

    Added multiple graphs and spreadsheets

---
+++ Makefile
@@ -0,0 +1,5 @@
+run:
+	source ./venv/bin/activate; \
+	export FLASK_APP=app.py; \
+	export FLASK_DEBUG=1; \
+	python3 -m flask run
--- app.py
+++ app.py
@@ -1,8 +1,90 @@
 from flask import Flask, current_app, jsonify, request
-from numpy import array
+from numpy import array, float64
 import LBC
+from werkzeug.utils import secure_filename
+import os
+import pandas as pd
+
+UPLOAD_FOLDER = './'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = ['xlsx', 'xls', 'csv', 'tsv']
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_file_extension(filename):
+    return filename.rsplit('.', 1)[1].lower()
+
+@app.route('/load', methods=['POST'])
+def upload_file():
+    print("Loading file")
+    # check if the post request has the file part
+    if 'input_file' not in request.files:
+        print("Input file missing")
+        return jsonify({
+            "success": False,
+            "message": "Input file missing"
+        })
+    file = request.files['input_file']
+    if file.filename == '':
+        print("Input file missing")
+        return jsonify({
+            "success": False,
+            "message": "Input file missing"
+        })
+    if (not file) or (not allowed_file(file.filename)):
+        print("Invalid file")
+        return jsonify({
+            "success": False,
+            "message": "Invalid file"
+        })
+    filename = secure_filename(file.filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+    print("File saved to %s" % filepath)
+
+    # Process the file
+    extension = get_file_extension(filename)
+    loaded = None
+    error_message = None
+    if extension == "csv" or extension == "tsv":
+        try:
+            loaded = pd.read_csv(filepath, header=None).to_numpy().astype(float64)
+        except:
+            # Try it without headers
+            try:
+                loaded = pd.read_csv(filepath).to_numpy().astype(float64)
+            except:
+                error_message = "csv is invalid or not fully numeric"
+    elif extension == "xlsx" or extension == "xls":
+        try:
+            loaded = pd.read_excel(filepath, header=None).to_numpy().astype(float64)
+        except Exception as e:
+            # Try it without headers
+            print("Excel error - ",e)
+            try:
+                loaded = pd.read_excel(filepath).to_numpy().astype(float64)
+            except Exception as e2:
+                print("Excel error 2 - ", e2)
+                error_message = "Excel file is invalid or not fully numeric"
+
+    if loaded is None:
+        return jsonify({
+            "success": False,
+            "message": error_message if error_message != None else "Could not read file"
+        })
+    else:
+        return jsonify({
+            "success": True,
+            "message": "File loaded",
+            "data": loaded.tolist()
+        })
+
+    # return redirect(url_for('uploaded_file',
+                            # filename=filename))
 
 @app.route('/synthetic_data', methods=['POST'])
 def synthetic_data():
@@ -12,7 +94,7 @@ def synthetic_data():
     data_params = request.get_json()
     for header in argument_headers:
         if header not in data_params:
-            return jsonify({"success":False, "message": "Missing parameter %s" % header})
+            return jsonify({"success":False, "message":  "Missing parameter %s" % header})
         arguments[header] = data_params[header] if header in argument_not_float else float(data_params[header]) 
 
     print("Arguments - %s" % arguments)
@@ -56,5 +138,4 @@ def index():
     return current_app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
